babc32484d938a0f54a0b367fb8781e28f6edd96
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-07-01 15:16:57 +0700

    a

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ parser.add_argument("-s", default="hamilton", choices=dict_solver.keys(),
                     help="name of the solver to direct the snake (default: hamilton)")
 parser.add_argument("-m", default="normal", choices=dict_mode.keys(),
                     help="game mode (default: normal)")
-parser.add_argument('--map', type=str, default=8,
+parser.add_argument('-map', type=str, default=8,
                     help="map size (default: 8x8)")
 args = parser.parse_args()
 
